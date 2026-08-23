c7af87f711f099073d53ef4f1b300abacb3583a4
A.Ardam <A.Ardam@etadbir.com>
2026-08-23 09:12:06 +0330

    modified state box style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import StatCard from "./components/StatCard/StatCard";
-
+import styles from "./App.module.css";
 const App = () => {
   return (
-    <div>
+    <div className={styles.grid}>
       <StatCard title="تراکنش های دیروز" value={200000} trend="up" />
       <StatCard title="تراکنش های امروز" value={1250} trend="down" />
       <StatCard title="تراکنش های فردا" value={50000} />
--- src/components/StatCard/StatCard.tsx
+++ src/components/StatCard/StatCard.tsx
@@ -1,18 +1,21 @@
-import "./StatCard.module.css";
+import styles from "./StatCard.module.css";
 
-type StartCardProps = {
+type StatCardProps = {
   title: string;
   value: number;
   trend?: "up" | "down";
 };
 
-const StatCard = ({ title, value, trend }: StartCardProps) => {
+const StatCard = ({ title, value, trend }: StatCardProps) => {
   return (
-    <div className="statCardStyle">
+    <div className={styles.statCardStyle}>
       <h3>{title}</h3>
-      <p>{value.toLocaleString("fa-IR")}</p>
-      {trend === "up" && <span className="upTrend">↑</span>}
-      {trend === "down" && <span className="downTrend">↓</span>}
+      <p>{value.toLocaleString("fa-IR")} ریال</p>
+      <p>
+        نمودار رشد :
+        {trend === "up" && <span className={styles.upTrend}>↑</span>}
+        {trend === "down" && <span className={styles.downTrend}>↓</span>}
+      </p>
     </div>
   );
 };
