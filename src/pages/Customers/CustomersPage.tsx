500cb33c27c3c7052a677f29c92f486ba87b60c4
A.Ardam <A.Ardam@etadbir.com>
2026-08-26 10:41:41 +0330

    fixed sum css properties for table

--- src/pages/Customers/CustomersPage.tsx
+++ src/pages/Customers/CustomersPage.tsx
@@ -8,51 +8,51 @@ const CustomersPage = () => {
     queryFn: customerApi.getAll,
   });
   return (
-    <>
-      <div id="customerContainer">
-        {isPending && <p>در حال بارگزاری مشتریان !!!</p>}
-        {isError && (
-          <div className="error">
-            <h3>{error.name}</h3>
-            <p>{error.message}</p>
-          </div>
-        )}
-        {!isPending && !isError && (
-          <table>
-            <thead className="theader">
-              <tr>
-                <th> عنوان</th>
-                <th>شناسه ملی</th>
-                <th>نام مستاجر</th>
-                <th>وضعیت</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.map((customer) => (
-                <tr key={customer.id}>
-                  <td>
-                    {customer.firstName} {customer.lastName}
-                  </td>
-                  <td>{customer.nationalCode}</td>
-                  <td>{customer.tenantName}</td>
-                  <td>
-                    {customer.isEnable ? (
-                      <span>فعال</span>
-                    ) : (
-                      <span>غیر فعال</span>
-                    )}
-                  </td>
-                  <td>
-                    <button>جزییات</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
+    <div className="container">
+      <div>
+        <h2>ادمین-مشتریان</h2>
       </div>
-    </>
+      <div className="card">فیلتر ها</div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th> عنوان</th>
+            <th>شناسه ملی</th>
+            <th>نام مستاجر</th>
+            <th>وضعیت</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {isPending && <p>در حال بارگزاری مشتریان !!!</p>}
+          {isError && (
+            <div className="error">
+              <h3>{error.name}</h3>
+              <p>{error.message}</p>
+            </div>
+          )}
+          {data?.map((customer) => (
+            <tr key={customer.id}>
+              <td>
+                {customer.firstName} {customer.lastName}
+              </td>
+              <td>{customer.nationalCode}</td>
+              <td>{customer.tenantName}</td>
+              <td>
+                {customer.isEnable ? (
+                  <span className="badge badgeActive">فعال</span>
+                ) : (
+                  <span className="badge badgeInactive">غیر فعال</span>
+                )}
+              </td>
+              <td>
+                <button className="button buttonPrimary">جزییات</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
